e9a62cef2b90240ab246f284fb5953519af68f3c
wmn7 <18811371255@163.com>
2023-12-02 18:08:12 +0800

    update ConventionalMethods to different scenarios

--- ConventionalMethods/sotl.py
+++ ConventionalMethods/sotl.py
@@ -2,7 +2,17 @@
 @Author: WANG Maonan
 @Date: 2023-12-01 20:58:40
 @Description: 感应控制算法
-@LastEditTime: 2023-12-01 22:43:39
+Command: 
+@ Scenario-1
+-> python sotl.py --env_name '3way' --phase_num 3
+-> python sotl.py --env_name '4way' --phase_num 4
+@ Scenario-2
+-> python sotl.py --env_name '3way' --phase_num 3 --edge_block 'E1'
+-> python sotl.py --env_name '4way' --phase_num 4 --edge_block 'E1'
+@ Scenario-3
+-> python sotl.py --env_name '3way' --phase_num 3 --detector_break 'E0--s'
+-> python sotl.py --env_name '4way' --phase_num 4 --detector_break 'E0--s'
+@LastEditTime: 2023-12-02 18:05:05
 '''
 import sys
 from pathlib import Path
@@ -11,22 +21,32 @@ parent_directory = Path(__file__).resolve().parent.parent
 if str(parent_directory) not in sys.path:
     sys.path.insert(0, str(parent_directory))
 
+import argparse
 from loguru import logger
 from tshub.utils.get_abs_path import get_abs_path
 from tshub.utils.init_log import set_logger
 
 from TSCEnvironment.tsc_env import TSCEnvironment
-from TSCEnvironment.llm_tsc_wrapper import LLMTSCEnvWrapper
+from TSCEnvironment.llm_wrapper import LLMTSCEnvWrapper
 
 path_convert = get_abs_path(__file__)
 set_logger(path_convert('./'))
 
 
 if __name__ == '__main__':
-    env_name = '4way'
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--env_name', type=str, default='4way', help='Environment name')
+    parser.add_argument('--phase_num', type=int, default=4, help='Phase number')
+    parser.add_argument('--edge_block', type=str, default=None, help='Edge block')
+    parser.add_argument('--detector_break', type=str, default=None, help='Detector break')
+
+    args = parser.parse_args()
+    env_name = args.env_name
+    phase_num = args.phase_num
+    edge_block = args.edge_block
+    detector_break = args.detector_break
+
     route_type = 'vehicle' # vehicle_pedestrian
-    tls_action_type = 'next_or_not' # next_or_not, choose_next_phase
-    phase_num = 4
     sumo_cfg = path_convert(f"../TSCScenario/{env_name}/env/{route_type}.sumocfg")
     net_file = path_convert(f"../TSCScenario/{env_name}/env/{env_name}.net.xml")
     log_path = path_convert(f'./')
@@ -36,9 +56,9 @@ if __name__ == '__main__':
         sumo_cfg=sumo_cfg, 
         net_file=net_file,
         trip_info=trip_info,
-        num_seconds=500,
+        num_seconds=1000,
         tls_id='J1', 
-        tls_action_type=tls_action_type,
+        tls_action_type='next_or_not',
         use_gui=True,
     ) # 初始化环境
     
@@ -52,8 +72,23 @@ if __name__ == '__main__':
     # Simulation with environment
     dones = False
     action = 1 # 初始的动作, 保持不变
+    sim_step = 0 # 初始仿真时间
     llm_env.reset()
     while not dones:
+        # 设置 edge block
+        if edge_block is not None:
+            if sim_step>50 and sim_step<100:
+                llm_env.set_edge_speed(edge_block, speed=1)
+            else:
+                llm_env.set_edge_speed(edge_block, speed=13)
+        
+        # 设置 detector break
+        if detector_break is not None:
+            if sim_step>200 and sim_step<400:
+                llm_env.set_occ_missing(not_work_element=detector_break) # 存在这 400s 的时候传感器数据是损坏的
+            else:
+                llm_env.set_occ_missing(not_work_element='') # 其余时间传感器数据是好的
+
         states, rewards, truncated, dones, infos = llm_env.step(action=action)
         this_phase_index = infos['this_phase_index']
         next_phase_index = (this_phase_index + 1)%phase_num
@@ -64,4 +99,6 @@ if __name__ == '__main__':
         else:
             action = 0 # 保持不变
             last_change_light_time = infos['step_time']
+        sim_step = infos['step_time']
+        logger.info(f'SIM: {infos}')
     llm_env.close()
